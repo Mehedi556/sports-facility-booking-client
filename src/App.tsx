484e365eb3fb77962f6811a81b143cb6929b8b38
Abdus Salam Mehedi <salam.mehedi99@gmail.com>
2024-08-25 13:00:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="max-w-full">
+      <div className="max-w-[1024px] mx-auto bg-gray-500">
+        <h1>Pera nai.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
